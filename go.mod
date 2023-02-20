f3d4cda95caf95a33f71cfa1ce8c1e3bd5d8007d
lrwx00t <lrwx00t@gmail.com>
2023-02-20 16:14:03 -0500

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/lrwx00t/golive
+
+go 1.19
+
+require github.com/fatih/color v1.14.1
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/fatih/color"
+	"github.com/fsnotify/fsnotify"
+)
+
+type RunnerOpts struct {
+	done *chan bool
+	path string
+	args []string
+}
+
+func runner(runnerOpts RunnerOpts) {
+	defer func() {
+		*runnerOpts.done <- true
+	}()
+	log.Println("started..")
+	cmd := exec.Command("go", append([]string{"run"}, runnerOpts.args...)...)
+	cmd.Dir = runnerOpts.path
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(color.RedString("Error: %v", err))
+		fmt.Print(color.RedString("%s", out))
+		return
+	}
+	fmt.Print(color.GreenString("%s", out))
+	log.Println("finished..")
+}
+
+func main() {
+	path := flag.String("path", "", "go project path")
+	pkg := flag.String("package", ".", "go package or file name")
+	flag.Parse()
+	if len(*path) == 0 {
+		log.Fatal("path cannot be empty")
+	}
+	done := make(chan bool)
+	runnerOpts := RunnerOpts{
+		done: &done,
+		path: *path,
+		args: []string{*pkg},
+	}
+	log.Println("simple web server has started..")
+
+	// create watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Start listening for events.
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				fmt.Println(event)
+				if !ok {
+					return
+				}
+				if event.Op.Has(fsnotify.Chmod) {
+					continue
+				}
+				go runner(runnerOpts)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+			<-*runnerOpts.done
+		}
+	}()
+
+	err = watcher.Add(runnerOpts.path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Block main goroutine forever.
+	<-make(chan struct{})
+}
