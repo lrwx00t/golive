aa95e229e61e1b82a3d3c448d46c2b00f36304c2
lrwx00t <lrwx00t@gmail.com>
2023-02-21 14:39:07 -0500

    support long running processes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # golive
 
-`golive` is a tool for real-time Go development, allowing you to see changes to your code as you make them. It automatically runs your code as you work and keeps everything in sync.
+`golive` is a tool for real-time Go development, allowing you to see changes to your code as you make them. It automatically runs your code as you work and keeps everything in sync. It currently provides limited support to `go run` only for short and long running processes.
 
 Whenever a change is detected in the directory, such as an updated or newly added file, the code will be executed automatically. If the execution is successful, the output will be displayed in green, otherwise any errors that occurred will be displayed in red. In all cases, `golive` is expected to continue running indefinitely until manually terminated by issuing a `SIGINT` signal e.g. `Ctrl + C`.
 
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
@@ -8,8 +9,11 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strconv"
+	"strings"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/fsnotify/fsnotify"
@@ -22,15 +26,84 @@ type RunnerOpts struct {
 
 func runner(ctx context.Context, wg *sync.WaitGroup, runnerOpts RunnerOpts) {
 	defer wg.Done()
+	runner_killer()
 	cmd := exec.Command("go", append([]string{"run"}, runnerOpts.args...)...)
 	cmd.Dir = runnerOpts.path
-	out, err := cmd.CombinedOutput()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		log.Printf("Error: %s", err)
-		fmt.Print(color.RedString("%s", out))
+		panic(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		panic(err)
+	}
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			os.Stdout.WriteString(color.GreenString("STDOUT: %s", scanner.Text()+"\n"))
+		}
+	}()
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			os.Stderr.WriteString(color.RedString("STDERR: %s", scanner.Text()+"\n"))
+		}
+	}()
+}
+
+func delete_empty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
+
+func runner_killer() {
+	mainPID := os.Getpid()
+	// TODO: Hack
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("ps aux | grep go-build | grep -v grep | grep -v %s | awk '{ print $2 }'", strconv.Itoa(mainPID)))
+
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error running command:", err)
 		return
 	}
-	fmt.Print(color.GreenString("%s", out))
+	outputStr := strings.TrimSpace(string(output))
+
+	pids := delete_empty(strings.Split(outputStr, "\n"))
+	if len(pids) == 0 {
+		return
+	}
+	for _, pidStr := range pids {
+		if pidStr != "" {
+			pid, err := strconv.Atoi(pidStr)
+			if err != nil {
+				fmt.Printf("Error parsing PID: %s\n", err)
+				return
+			}
+			killCmd := exec.Command("kill", strconv.Itoa(pid))
+			err = killCmd.Start()
+			if err != nil {
+				fmt.Printf("Error killing process %d: %s\n", pid, err)
+			} else {
+				err := killCmd.Wait()
+				if err != nil {
+					fmt.Printf("Error waiting for process %d to exit: %s\n", pid, err)
+				} else {
+					fmt.Printf("Killed process %d\n", pid)
+				}
+			}
+		}
+		fmt.Println("pid:", pidStr)
+	}
+	runner_killer()
 }
 
 func main() {
@@ -46,6 +119,8 @@ func main() {
 	}
 	defer watcher.Close()
 
+	t := time.Now()
+
 	go func() {
 		wg := sync.WaitGroup{}
 		for {
@@ -54,15 +129,20 @@ func main() {
 				if !ok {
 					return
 				}
-				if event.Op.Has(fsnotify.Chmod) {
+				if event.Op.Has(fsnotify.Chmod) || strings.HasSuffix(event.Name, ".swp") {
 					continue
 				}
-				wg.Wait()
+				if time.Since(t).Seconds() <= 1.0 {
+					continue
+				}
+				fmt.Println(event)
+				t = time.Now()
 				wg.Add(1)
 				go runner(context.Background(), &wg, RunnerOpts{
 					path: *path,
 					args: []string{*pkg},
 				})
+				wg.Wait()
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
